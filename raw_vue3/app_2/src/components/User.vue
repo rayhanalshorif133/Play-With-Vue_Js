e383e7ba5f483d02a746c51eeb7825360e1ca285
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-01-01 15:26:10 +0600

    working on selected

--- raw_vue3/app_2/src/components/User.vue
+++ raw_vue3/app_2/src/components/User.vue
@@ -1,8 +1,10 @@
 <script>
 import Header from "./Header.vue";
+import Button from "./common/Button.vue";
 export default {
   components: {
     Header,
+    Button,
   },
   data() {
     return {
@@ -42,15 +44,17 @@ export default {
             class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
           >
             <tr>
+              <th scope="col" class="px-6 py-3">
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-4 w-4 cursor-pointer text-indigo-600 transition duration-150 ease-in-out"
+                />
+              </th>
               <th scope="col" class="px-6 py-3">#List</th>
               <th scope="col" class="px-6 py-3">Photo</th>
               <th scope="col" class="px-6 py-3">User name</th>
               <th scope="col" class="px-6 py-3">Email</th>
-              <th scope="col" class="px-6 py-3">Category</th>
-              <th scope="col" class="px-6 py-3">Price</th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
+              <th scope="col" class="px-6 py-3">Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -59,6 +63,15 @@ export default {
               :key="user.id"
               class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
             >
+                <th
+                    scope="row"
+                    class="px-6 py-4 whitespace-nowrap text-sm font-medium cursor-pointer text-gray-900 dark:text-white" 
+                >
+                    <input
+                        type="checkbox"
+                        class="form-checkbox cursor-pointer h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                    />
+                </th>
               <th
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -78,14 +91,12 @@ export default {
                 {{ user.firstName }} {{ user.lastName }}
               </th>
               <td class="px-6 py-4">{{ user.email }}</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
               <td class="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >Edit</a
-                >
+                <div class="flex space-x-1">
+                  <Button icon="fa-solid fa-eye" bgColor="bg-teal-600" hoverBgColor="bg-teal-500"/>
+                  <Button icon="fa-solid fa-pen-to-square" bgColor="bg-sky-600" hoverBgColor="bg-sky-500"/>
+                  <Button icon="fa-solid fa-trash" bgColor="bg-red-600" hoverBgColor="bg-red-500"/>
+                </div>
               </td>
             </tr>
           </tbody>
---
+++ raw_vue3/app_2/src/components/common/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    :class="`block font-medium ${bgColor} py-1 px-2 cursor-pointer rounded-md text-white hover:${hoverBgColor}`"
+  >
+    <i :class="icon"></i>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ["icon", "bgColor","hoverBgColor"],
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
